566c273556f0aa5074d7cb5a0f54e2d337653474
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-22 05:13:42 +0800

    page routing setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Layout from './components/Layout';
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from './pages/Projects';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Layout>
-      <h1>Content</h1>
-    </Layout>
+    <div className="App">
+     <BrowserRouter>
+     
+       <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+
+       </Routes>
+     
+     </BrowserRouter>
+    </div>
   );
 }
 
