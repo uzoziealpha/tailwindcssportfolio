bee69f6efff19c5637bdbb561ad8635677d05864
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-22 04:42:38 +0800

    Tailwind setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hi</h1>
+     <button className='bg-red-500'>tail</button>
+    </div>
+  );
+}
+
+export default App;
