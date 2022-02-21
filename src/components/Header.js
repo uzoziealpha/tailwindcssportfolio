52c3002558a141e77f1870dc2c25a30d695f4085
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-22 06:38:40 +0800

    Responsive tailwind for nav toggler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+    //hooks
+  const [showMenu, setShowMenu] = useState('md:hidden')  
   const menuItems = [
     {
-        title: 'Home',
-        key: '/'
+      title: "Home",
+      key: "/",
     },
     {
-        title: 'Projects',
-        key: '/projects'
+      title: "Projects",
+      key: "/projects",
     },
     {
-        title: 'Contact',
-        key: '/contact'
+      title: "Contact",
+      key: "/contact",
     },
-  ]
-  
-    return (
-    <div className='text-white font-pop'>
-      <div className='flex bg-theme justify-between items-center p-2 shadow-lg'>
-          <div>
-              <h1 className='text-4xl font-semibold'>OBI</h1>
-          </div>
-          <div className='flex'>
-              {menuItems.map((item) => {
-                 return (
-                     <li className='list-none mx-5'>
-                     <Link to={`/${item.key}`}>
-                       {item.title}
-                     </Link>
-                 </li>
-                );
-              })}
-          </div>
-      </div>
+  ];
 
+  return (
+    <div className="text-white font-pop">
+      <div className="flex bg-theme justify-between items-center p-2 shadow-lg">
+        <div className='flex justify-between w-full'>
+          <h1 className="text-4xl font-semibold">OBI</h1>
+         <button
+           onClick={()=> {
+             setShowMenu();
+           }}
+          className='lg:hidden xl:hidden 2xl:hidden md:flex'>MENU</button>
+        </div>
+        <div className="flex md:hidden">
+          {menuItems.map((item) => {
+            return (
+              <li className="list-none mx-5">
+                <Link to={`/${item.key}`}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
