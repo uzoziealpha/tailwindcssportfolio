0ca3be8072e6b375c2f2fe52c8865be619b51cca
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-22 21:11:29 +0800

    render static data with projects and hover

---
+++ src/rescources/projects.js
@@ -0,0 +1,39 @@
+const projectsData = [
+  {
+    title: "MEALSTOGO",
+    image: "./proj1.png",
+    description:
+      "An ecommerce application developed using react & react-native ",
+    link: "/",
+  },
+  {
+    title: "RICE ROBOTICS",
+    image: "./proj4.png",
+    description:
+      "An ecommerce application developed using react , node , redux and firebase/firestore",
+    link: "/",
+  },
+  {
+    title: "SHEYROOMS",
+    image: "./proj3.png",
+    description:
+      "An ecommerce application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+  {
+    title: "AI ROBO API ",
+    image: "./proj2.png",
+    description:
+      "An  application developed using react , node , redux and mongodb",
+    link: "/",
+  },
+  {
+    title: "NASA Habitable planets ",
+    image: "./proj5.png",
+    description:
+      "An application developed using react , node  and mongodb",
+    link: "/",
+  },
+];
+
+export default projectsData;
