900ae0b7995f90df90a1388faaf6eeec8e715a85
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-22 21:46:33 +0800

    contact section and form input

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,28 @@ function Contact() {
   return (
     <Layout>
       <div>
-        <h1> Contact ME </h1>
+        <div className="h-screen">
+        <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_u25cckyh.json"  background="transparent"  speed="1.5" 
+         loop 
+          autoplay>
+        </lottie-player>
+        </div>
+
+        <div>
+           <div className='w-screen flex justify-center'>
+
+           <div className='md:w-full w-1/2 p-10 shadow-2x1 bg-gray-50'>
+           <h1 className='text-2x1 font-semibold'>Contact Me</h1>
+             <input type="text" placeholder='Name' className='w-full border-2  border-gray-600 rounded p-1 shadow-lg mt-5'/>
+             <input type="text" placeholder='Email' className='w-full border-2  border-gray-600 rounded p-1 shadow-lg mt-5'/>
+             <textarea type="text" placeholder='Message' className='w-full border-2  border-gray-600 rounded p-1 shadow-lg mt-5'/>
+
+             <button className='bg-blue-600 rounded text-white px-5 py-1 mt-3'>SUBMIT</button>
+           </div>
+
+
+       </div>
+        </div>
       </div>
     </Layout>
   );
